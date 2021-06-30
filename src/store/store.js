a94356a850a179fd3a06d09945ee0169a5743aef
unknown <sergio-caballero_student2021@wilder.school>
2021-06-30 21:17:32 +0200

    configuring redux

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,21 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import { authReducer } from '../reducers/authReducer';
+
+
+
 
 //let us to combine several reducers in order to use them in the store
 const reducers =combineReducers({
     auth: authReducer
 })
 
+//Config to apply different middelwares
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
 export const store= createStore(
     reducers, 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 );
