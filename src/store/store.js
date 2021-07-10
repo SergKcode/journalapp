ad3866965df226c9fe9b15892aa48594eb0e2365
unknown <sergio-caballero_student2021@wilder.school>
2021-07-10 12:50:04 +0200

    uiReducer created

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,14 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+
 import { authReducer } from '../reducers/authReducer';
-
-
+import { uiReducer } from '../reducers/uiReducer';
 
 
 //let us to combine several reducers in order to use them in the store
 const reducers =combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    ui: uiReducer
 })
 
 //Config to apply different middelwares
